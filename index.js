35879e1dac6ec600c52b0ad9e7cbb10f2161a3fe
alvin <maulanaalvin1@gmail.com>
2023-12-06 01:35:23 +0800

    Add CORS handling to the server

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// handle cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        '*'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS')
+        return res.status(200).json({});
+    }
+    next();
+});
+
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
